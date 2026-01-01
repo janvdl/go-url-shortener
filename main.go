dafa85107ea81bdf0f27472d094006cf5a597a7b
Jan van der Linde <jvdl@jvdl.me>
2026-01-01 14:29:27 +0000

    add ability to retrieve full url from DB via shorturl key

--- main.go
+++ main.go
@@ -46,19 +46,34 @@ func getHome(c *gin.Context) {
 
 // get full URL from short URL
 func getURL(c *gin.Context) {
+	// get short url param
+	shorturl := c.Param("shorturl")
 
+	// retrieve full url from DB
+	fullurl, err := rdb.Get(ctx, shorturl).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	// return full url
+	c.JSON(http.StatusOK, gin.H{
+		"fullurl": fullurl,
+	})
 }
 
 // testing functionality in browser for short url and
 // DB connection
 func newURL(c *gin.Context) {
+	// get new short link
 	shorturl := makeShortLink()
 
+	// test DB functionality and add google.com as placeholder for now
 	err := rdb.Set(ctx, shorturl, "https://www.google.com", 0).Err()
 	if err != nil {
 		panic(err)
 	}
 
+	// return new shortlink
 	c.JSON(http.StatusOK, gin.H{
 		"shorturl": shorturl,
 	})
